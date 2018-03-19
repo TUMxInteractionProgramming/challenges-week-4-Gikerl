e21fb3178d3fd55e3bc70475a79867622848300a
Gikerl <12859583+Gikerl@users.noreply.github.com>
2018-03-19 16:28:54 +0100

    Add files via upload
    Part 1

---
+++ js/channels.js
@@ -0,0 +1,52 @@
+/* #6 start the #external #action and say hello */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/*jslint devel: true */
+/*jslint browser: true*/
+
+
+var channel1 = {
+    name: '#Yummy',
+    createdOn: new Date('April 1, 2016 12:00:00'),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 6000,
+    messageCount: 999
+};
+
+var channel2 = {
+    name: '#SevenContinents',
+    createdOn: new Date('April 1, 2016 12:00:00'),
+    createdBy: "UPGRADING.NEVER.HELPS",
+    starred: true,
+    expiresIn: 6000,
+    messageCount: 999
+};
+
+var channel3 = {
+    name: '#KillerApp',
+    createdOn: new Date('April 1, 2016 12:00:00'),
+    createdBy: "UPGRADING.NEVER.HELPS",
+    starred: false,
+    expiresIn: 6000,
+    messageCount: 999
+};
+
+var channel4 = {
+    name: '#FirstPersonOnMars',
+    createdOn: new Date('April 1, 2016 12:00:00'),
+    createdBy: "UPGRADING.NEVER.HELPS",
+    starred: true,
+    expiresIn: 6000,
+    messageCount: 999
+};
+
+var channel5 = {
+    name: '#Octoberfest',
+    createdOn: new Date('April 1, 2016 12:00:00'),
+    createdBy: "UPGRADING.NEVER.HELPS",
+    starred: false,
+    expiresIn: 6000,
+    messageCount: 999
+};
--- js/script.js
+++ js/script.js
@@ -1,32 +1,65 @@
 /* #6 start the #external #action and say hello */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/*jslint devel: true */
+/*jslint browser: true*/
+
+
+/*global $, jQuery, alert*/
 console.log("App is alive");
+var currentChannel = channel2;
+var currentLocation = {
+    Latitude: 48.137154,
+    Longitude: 11.576124,
+    W3W: "lose.anpassung.loch"
+};
+
+
 
 /**
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
-function switchChannel(channelName) {
+function switchChannel(channel) {
+    currentChannel = channel;
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", currentChannel.name);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = currentChannel.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    var html = 'by <a href="http://w3w.co/' + currentChannel.createdBy + '" target="_blank"><strong>' + channel.createdBy + '</strong></a>';
+    document.getElementById('channel-location').innerHTML = html;
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    if (currentChannel.starred) {
+        $('#channel-star').html('<i id="channel-star" onclick="star()" class="fas fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
+    } else {
+        $('#channel-star').html('<i id="channel-star" onclick="star()" class="far fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
+    }
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channel.name + ')').addClass('selected');
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    if (currentChannel.starred) {
+        $('#channel-star').html('<i id="channel-star" onclick="star()" class="far fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
+        $('#channels li:contains(' + currentChannel.name + ') div').html('<i class="far fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
+        currentChannel.starred = false;
+        console.log('UnFavorite');
+    } else {
+        $('#channel-star').html('<i id="channel-star" onclick="star()" class="fas fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
+        $('#channels li:contains(' + currentChannel.name + ') div').html('<i class="fas fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
+        currentChannel.starred = true;
+        console.log('Favorite');
+    }
+    event.stopPropagation();
 }
 
 /**
