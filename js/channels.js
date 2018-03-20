00c888ebe2850b3a184fe4c72da4158bc7060400
Gikerl <12859583+Gikerl@users.noreply.github.com>
2018-03-20 15:38:29 +0100

    Add files via upload
    Challenge 2

--- js/channels.js
+++ js/channels.js
@@ -6,7 +6,19 @@
 /*jslint browser: true*/
 
 
+/*
+function Channel(name, createdOn, createdBy, starred, expiresIn, messageCount) {
+    this.name = name;
+    this.createdOn = createdOn;
+    this.createdBy = createdBy;
+    this.starred = starred;
+    this.expiresIn = expiresIn;
+    this.messageCount = messageCount;
+}
+*/
+
 var channel1 = {
+    id: 1,
     name: '#Yummy',
     createdOn: new Date('April 1, 2016 12:00:00'),
     createdBy: "minus.plus.yummy",
@@ -16,6 +28,7 @@ var channel1 = {
 };
 
 var channel2 = {
+    id: 2,
     name: '#SevenContinents',
     createdOn: new Date('April 1, 2016 12:00:00'),
     createdBy: "UPGRADING.NEVER.HELPS",
@@ -25,6 +38,7 @@ var channel2 = {
 };
 
 var channel3 = {
+    id: 3,
     name: '#KillerApp',
     createdOn: new Date('April 1, 2016 12:00:00'),
     createdBy: "UPGRADING.NEVER.HELPS",
@@ -34,6 +48,7 @@ var channel3 = {
 };
 
 var channel4 = {
+    id: 4,
     name: '#FirstPersonOnMars',
     createdOn: new Date('April 1, 2016 12:00:00'),
     createdBy: "UPGRADING.NEVER.HELPS",
@@ -43,6 +58,7 @@ var channel4 = {
 };
 
 var channel5 = {
+    id: 5,
     name: '#Octoberfest',
     createdOn: new Date('April 1, 2016 12:00:00'),
     createdBy: "UPGRADING.NEVER.HELPS",
@@ -50,3 +66,27 @@ var channel5 = {
     expiresIn: 6000,
     messageCount: 999
 };
+
+function listChannels() {
+    var channels = [channel1, channel2, channel3, channel4, channel5];
+    for (var i = 0; i < channels.length; i++) {
+        $(createChannelElement(channels[i])).appendTo('#channels ul');
+    }
+
+}
+
+function createChannelElement(channelObject) {
+    console.log(channelObject.variablename
+               )
+    var star = "far fa-star";
+    if (channelObject.starred) {
+        star = "fas fa-star";
+    }
+    var appendString = '<li onclick="switchChannel(channel' + channelObject.id + ')">' + channelObject.name + '<span class="channel-meta"></span><div class="channel-meta" style="font-size:24px; color:black"><i class="star ' + star + '"></i><span style="background-color: #3F51B5; color: white; font-size: 10px; font-weight: bold; padding: 2px; margin: 0px 2px; display:inline-block; vertical-align:middle; border-radius:4px;">' + channelObject.expiresIn / 60 + ' min</span><span style="background-color: #3F51B5; color: white; font-size: 10px; font-weight: bold; padding: 2px; margin: 0px 2px; display:inline-block; vertical-align:middle; border-radius:4px;">' + channelObject.messageCount + '</span><i class="fas fa-chevron-right"></i></div></li>';
+    
+    return appendString
+}
+
+$(document).ready(function () {
+    listChannels();
+});
--- js/script.js
+++ js/script.js
@@ -50,14 +50,24 @@ function switchChannel(channel) {
 function star() {
     if (currentChannel.starred) {
         $('#channel-star').html('<i id="channel-star" onclick="star()" class="far fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
-        $('#channels li:contains(' + currentChannel.name + ') div').html('<i class="far fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
+        //$('#channels li:contains(' + currentChannel.name + ') div').html('<i class="far fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
         currentChannel.starred = false;
         console.log('UnFavorite');
     } else {
         $('#channel-star').html('<i id="channel-star" onclick="star()" class="fas fa-star" style="display: inline-block; position: absolute; right: 16px; color: black; font-size: 24px;"></i>');
-        $('#channels li:contains(' + currentChannel.name + ') div').html('<i class="fas fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
+        //$('#channels li:contains(' + currentChannel.name + ') div').html('<i class="fas fa-star"></i>\n<i class="fas fa-chevron-right"></i>');
+
+
         currentChannel.starred = true;
         console.log('Favorite');
+    }
+    var icon = $('#channels li:contains(' + currentChannel.name + ') div .star');
+    var icon_fa_prefix = icon.attr('data-prefix');
+    if (icon_fa_prefix === "fas") {
+        icon.attr('data-prefix', 'far');
+
+    } else {
+        icon.attr('data-prefix', 'fas');
     }
     event.stopPropagation();
 }
@@ -83,4 +93,62 @@ function selectTab(tabId) {
 function toggleEmojis() {
     /* $('#emojis').show(); // #show */
     $('#emojis').toggle(); // #toggle
+}
+
+/*
+Message Constructor
+*/
+function Message(text) {
+    this.CreatedBy = currentLocation.W3W;
+    this.Latitude = currentLocation.Latitude;
+    this.Longitude = currentLocation.Longitude;
+    this.CreatedOn = new Date(Date.now());
+    this.ExpiresOn = new Date(+Date.now() + (15 * 60 * 1000));
+    this.Text = text;
+    this.Own = true;
+}
+
+function createMessageElement(messageObject) {
+    //Set Options for Date Display
+    var dateOptions = {
+        timeZone: 'Europe/Berlin',
+        weekday: 'short',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    };
+
+    //Calculate Minutes left
+    var minutesExpired = Math.round((((messageObject.ExpiresOn - messageObject.CreatedOn) % 86400000) % 3600000) / 60000);
+
+    //Assign the correct Class
+    var messageClass = 'message';
+    if (messageObject.Own) {
+        messageClass = 'message own';
+    }
+
+    //Create the messageString
+    var messageString =
+        '<div class=\"' + messageClass + '\"><h3><a href=\"' + 'http://w3w.co/' + messageObject.CreatedBy + '\" target=\"_blank\"><strong>' + messageObject.CreatedBy + '</strong></a> ' +
+        messageObject.CreatedOn.toLocaleString('en-GB', dateOptions) + ' <em>' + minutesExpired +
+        ' min.left </em></h3><p>' + messageObject.Text + '</p><button> +5 min. </button></div>';
+
+    return messageString;
+}
+
+function sendMessage() {
+    //Read the message and save it locally
+    var message = new Message($('#message').val());
+
+    console.log(message);
+
+    //Append Message to Div
+    $(createMessageElement(message)).appendTo('#messages');
+
+    //Scroll to the bottem
+    $('#messages').scrollTop($('#messages').height());
+
+    //Empty the Input-field
+    $('#message').val("");
 }
